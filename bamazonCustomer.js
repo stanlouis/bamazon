3b9f0e68625f8aadf786ff79d1bce2bf038ffd81
Stanley Louis <stanlouis@msn.com>
2018-06-23 15:36:21 -0400

    Challenge #1 rev0 complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,9 +25,12 @@ connection.connect(function (err) {
 function displayAllProducts() {
   connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
     if (err) throw err;
+    console.log('---Our list of products---');
+
     for (var i = 0; i < res.length; i++) {
       console.log("Item_id: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Price: " + res[i].price);
     }
+    console.log('\n');
     purchase();
   });
 }
@@ -56,6 +59,42 @@ function purchase() {
     ]).then(function (input) {
       var item = input.item;
       var quantity = input.quantity;
-      console.log('item:', item, 'quantity:', quantity)
+      // console.log('item:', item, 'quantity:', quantity)
+      var query = 'SELECT item_id, stock_quantity, price FROM products WHERE ?';
+
+      connection.query(query, {
+          item_id: item
+        },
+        function (err, res) {
+          var itemId = res[0].item_id;
+          var quantityRemaining = res[0].stock_quantity;
+          var price = res[0].price;
+          // console.log(res, itemId, quantityRemaining)
+          if (res.length === 0) {
+            console.log(`Sorry, we do not carry item with item_id ${item}`);
+            displayAllProducts();
+          } else if (quantityRemaining < quantity) {
+            console.log(`Insufficient quantity!, ${quantityRemaining} left`);
+            displayAllProducts();
+          } else {
+            var cost = quantity * price;
+            quantityRemaining -= quantity;
+            console.log(`Thanks for your purchase. Your total cost is $${cost}`);
+            connection.query(
+              "UPDATE products SET ? WHERE ?", [{
+                  stock_quantity: quantityRemaining
+                },
+                {
+                  item_id: itemId
+                }
+              ],
+              function (error) {
+                if (error) throw err;
+                console.log("Quantity successfully updated!");
+                displayAllProducts();
+              }
+            );
+          }
+        })
     })
 }
