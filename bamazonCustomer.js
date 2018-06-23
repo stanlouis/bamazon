275bb47ccd2233dfd0aa29275e2d7da1ba76751d
Stanley Louis <stanlouis@msn.com>
2018-06-23 14:28:34 -0400

    Add prompt to receive order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,8 +16,46 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  displayAllProducts();
 });
+
+function displayAllProducts() {
+  connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item_id: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Price: " + res[i].price);
+    }
+    purchase();
+  });
+}
+
+function validateEntry(value) {
+  if (isNaN(value) === false) {
+    return true;
+  }
+  return "Please enter a valid Number";
+}
+
+function purchase() {
+  inquirer
+    .prompt([{
+        name: "item",
+        type: "input",
+        message: "What is The item_id you would like to purchase?",
+        validate: validateEntry
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many units of the product would would like to buy?",
+        validate: validateEntry
+      }
+    ]).then(function (input) {
+      var item = input.item;
+      var quantity = input.quantity;
+      console.log('item:', item, 'quantity:', quantity)
+    })
+}
